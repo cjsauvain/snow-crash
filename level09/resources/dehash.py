1a15f79f1a644644b2ff105c27a813bc3e797fd7
Pierre Rabaud <pirabaud@3G2.42angouleme.fr>
2025-04-04 15:57:34 +0200

    refacto script

--- level09/resources/dehash.py
+++ level09/resources/dehash.py
@@ -1,12 +1,16 @@
-hex_string = "66346b6d6d36707c3d827f70826e838244428344757b7f8c890a"
 
-decoded_string = ""
-for i in range(0, len(hex_string), 2):
-    hex_value = hex_string[i:i+2]
-    num = int(hex_value, 16)
-    index = i // 2
-    original_num = (num - (index)) % 177
-    decoded_string += chr(original_num)
 
-print(decoded_string)
+try:
+    hex_string = input("token: ")
+    decoded_string = ""
+    for i in range(0, len(hex_string), 2):
+        hex_value = hex_string[i:i+2]
+        num = int(hex_value, 16)
+        index = i // 2
+        original_num = (num - (index)) % 177
+        decoded_string += chr(original_num)
+
+    print(decoded_string[:-1])
+except Exception as e:
+    print(e)
 
